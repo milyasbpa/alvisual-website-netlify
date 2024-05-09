de10ebeb8aa25836b05b474d0a3f3cc4cb3753f7
milyasbpa <basilyas.2015@gmail.com>
2024-05-09 16:14:00 +0700

    fix: banner components

--- src/core/ui/components/banner/Banner.component.tsx
+++ src/core/ui/components/banner/Banner.component.tsx
@@ -7,14 +7,14 @@ export interface BannerProps {
 
 export default function Banner({ image = "", alt = "" }: BannerProps) {
   return (
-    <div className={clsx("w-full h-[166px] md:h-[40rem]", "relative")}>
-      <img
-        className={clsx("object-cover")}
-        src={image}
-        alt={alt}
-        loading={"eager"}
-        sizes={"100vw"}
-      />
+    <div
+      className={clsx(
+        "w-full h-[166px] md:h-[40rem]",
+        "relative",
+        "overflow-hidden"
+      )}
+    >
+      <img src={image} alt={alt} loading={"eager"} sizes={"100vw"} />
     </div>
   );
 }
